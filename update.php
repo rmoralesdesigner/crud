726a8c213fdd510dd6d46ca2c913fcd8c1219c6d
Ramon Morales <rmoralesdesign@gmail.com>
2020-11-29 17:51:35 -0300

    adicionando icones aos botoes

--- update.php
+++ update.php
@@ -128,8 +128,12 @@ if (!empty($_POST)) {
         
         <div class="d-flex justify-content-between align-items-center col-12 mt-5">
         
-        <button type="submit" class="btn btn-success btn-lg rounded-pill text-uppercase px-5">Salvar</button>
-        <a href="index.php" type="btn" class="text-light">Voltar</a>
+        <button type="submit" class="btn btn-success btn-lg rounded-pill px-4"><svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-person-check mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10zm4.854-7.85a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+</svg> Salvar</button>
+        <a href="index.php" type="btn" class="text-light"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+</svg> Voltar</a>
 
         </div>
 
