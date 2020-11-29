90000491223922a7411d0d08fddda0949fb05b9d
Ramon Morales <rmoralesdesign@gmail.com>
2020-11-29 17:59:09 -0300

    adicionando icones aos botoes

--- delete.php
+++ delete.php
@@ -44,8 +44,14 @@ if(!empty($_POST))
         <input type="hidden" name="id" value="<?php echo $id;?>" />
 
         <div class="form actions">
-            <button type="submit" class="btn btn-danger btn-lg rounded-pill text-uppercase px-5">Sim</button>
-            <a href="index.php" type="btn" class="btn btn-default text-light">Não</a>
+            <button type="submit" class="btn btn-danger btn-lg rounded-pill px-4"><svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-check-circle mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+</svg> Sim</button>
+            <a href="index.php" type="btn" class="btn btn-default text-light"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg> Não</a>
         </div>
 
     </form>
